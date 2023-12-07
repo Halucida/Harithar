37e279ee83f83b7df2c37abb2054f96ea5840fb2
Kepin <141326351+Internite@users.noreply.github.com>
2023-12-07 22:55:37 +0700

    Hii !

---
+++ Helper.py
@@ -0,0 +1,11 @@
+import pickle
+import tensorflow as tf
+
+def load_tokenil(tokenil_path):
+    return tokenil
+
+def unlist(inputs):
+    return output
+
+def untider(tokenil, inputs, **untiargs):
+    return result
