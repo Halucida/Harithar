283518add68d7a4d61e9361ac1dbc5cc6ab580aa
Kepin <141326351+Internite@users.noreply.github.com>
2023-12-09 01:06:26 +0700

    Hii !

--- Helper.py
+++ Helper.py
@@ -1,18 +1,26 @@
 import pickle
 import tensorflow as tf
 
+from stop_words import get_stop_words
+
 def tokenil(filepath):
     tokeniler = pickle.load()
     return tokenil
 
 def unlist(inputs):
+    remope = get_stop_words("en")
+    inputs = inputs.lower().split()
+    output = " ".join([i for i in inputs if i not in remope])
     return output
 
-def untider(tokenil, inputs, **untiargs):
+def untider(inputs, **untiargs):
+    tokeni = tokenil()
+    sequen = tokenir.texts_to_sequence(inputs)
+    result = tf.keras.utils.pad_sequence(sequen, **untiargs)
     return result
 
 def modelator(rinput : str):
-    result = untider(tokenil, maxlen=maxlen, padding="post")
+    result = untider(rinput, maxlen=maxlen, padding="post")
     return result
 
 def modelrunner(rinput):
