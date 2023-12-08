8d9ebd7fc419299023526be0289c91237997da02
Kepin <141326351+Internite@users.noreply.github.com>
2023-12-09 00:46:53 +0700

    Hii !

--- Helper.py
+++ Helper.py
@@ -2,11 +2,21 @@ import pickle
 import tensorflow as tf
 
 def tokenil(filepath):
-    tokeniler = 
+    tokeniler = pickle.load()
     return tokenil
 
 def unlist(inputs):
     return output
 
 def untider(tokenil, inputs, **untiargs):
+    return result
+
+def modelator(rinput : str):
+    result = untider(tokenil, maxlen=maxlen, padding="post")
+    return result
+
+def modelrunner(rinput):
+    model = tf.keras.models.load_model()
+    data = modelator(rinput)
+    result = model.predict(data)
     return result
